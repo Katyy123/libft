4f0c66e9e5890bbeda0eebc7aec4b9f8b91a4ae4
Catia Filiberto <cfiliber@e3r10p1.42roma.it>
2022-06-28 18:59:54 +0200

    ok

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 19:41:51 by cfiliber          #+#    #+#             */
-/*   Updated: 2021/03/04 17:04:15 by cfiliber         ###   ########.fr       */
+/*   Updated: 2021/10/29 19:14:36 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list *new;
 
-	if (!(new = (t_list *)malloc(sizeof(t_list))))
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!(new))
 		return (0);
 	new->content = content;
 	new->next = NULL;
